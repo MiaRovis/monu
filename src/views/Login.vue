5db776d82771066823d7bf323b37cd9b6b0a5f08
Mia Rovis <mia.rovis@gmail.com>
2023-08-23 18:25:12 +0200

    commit to master

---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="about">
+    <div class="container">
+    <div class="row">
+    <div class="col-sm"></div>
+    <div class="col-sm">
+    <br/>
+
+    <form>
+    <div class="form-group">
+    <p id="admin"><b>Log-In</b></p>
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    </div>
+
+    <div id="loz" class="form-group">
+    <label for="exampleInputPassword2">Password</label>
+    <br/>
+    <input type="password"  class="form-control" id="exampleInputPassword1" placeholder="Password" />
+    <br/>
+    </div>
+ 
+    <button type="button"  class="btn btn-secondary">Submit</button>
+    <br/>
+    
+    </form>
+    </div>
+    <div class="col-sm"></div>
+    </div>
+    </div>
+    </div>
+    </template>
+
+<style lang="scss">
+
+#admin{
+    font-size:30px;
+    color:rgb(23, 23, 24);
+    
+}
+
+
+</style>
